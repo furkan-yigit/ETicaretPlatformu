d54b11c941fcfa021e1c885293b3869b5ef0bb21
Yalın <yalintuzmen08@gmail.com>
2024-04-01 15:24:45 +0300

    interface eklendi

--- ETicaretPlatformu.Application/Services/OrderDetailService/IOrderDetailService.cs
+++ ETicaretPlatformu.Application/Services/OrderDetailService/IOrderDetailService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ETicaretPlatformu.Application.Models.DTOs.Order_Details;
+using ETicaretPlatformu.Application.Models.VMs.Order;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,9 @@ namespace ETicaretPlatformu.Application.Services.OrderDetailService
 {
     public interface IOrderDetailService
     {
+        Task Create(CreateOrderDetailDto model);
+        Task<OrderDetailVm> GetOrderDetailByOrderId(int orderId);
+        Task Update(UpdateOrderDetailDto model);
+        Task Delete(int orderDetailsId);
     }
 }
