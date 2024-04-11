97424093a494c78b5e113f678e36cbb9c01c9461
Yalın <yalintuzmen08@gmail.com>
2024-04-11 18:39:56 +0300

    table bozulmuş düzelttim

--- ETicaretPlatformu.Application/Models/DTOs/Order/UpdateOrderDto.cs
+++ ETicaretPlatformu.Application/Models/DTOs/Order/UpdateOrderDto.cs
@@ -15,7 +15,7 @@ namespace ETicaretPlatformu.Application.Models.DTOs.OrderDto
         public string UserId { get; set; }
         public OrderStatus OrderStatus { get; set; }
         public DateTime CreateDate { get; set; }
-        public ICollection<OrderDetail> OrderDetails { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
 
     }
 }
