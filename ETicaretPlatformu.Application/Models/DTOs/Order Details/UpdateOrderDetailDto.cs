7e508734fb544dd4ad2951c01d0f34d2753c04f6
Yalın <yalintuzmen08@gmail.com>
2024-04-14 19:03:38 +0300

    attributeları verdim

--- ETicaretPlatformu.Application/Models/DTOs/Order Details/UpdateOrderDetailDto.cs
+++ ETicaretPlatformu.Application/Models/DTOs/Order Details/UpdateOrderDetailDto.cs
@@ -1,6 +1,7 @@
 ﻿using ETicaretPlatformu.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,11 @@ namespace ETicaretPlatformu.Application.Models.DTOs.Order_Details
     public class UpdateOrderDetailDto
     {
         public int Id { get; set; }
-        public int OrderId { get; set; }
-        public int ProductId { get; set; }
 
+        public int OrderId { get; set; }
+        [Required (ErrorMessage = "Product is required!")]
+        public int ProductId { get; set; }
+        [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
     }
 }
